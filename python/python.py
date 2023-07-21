416f42f6d91014605282e7eab1325b00fcfd509f
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-07-21 09:26:01 +0200

    update

--- python/python.py
+++ python/python.py
@@ -72,7 +72,7 @@ if __name__ == '__main__':
         print(' i = %s' % counter)
         writPath = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
         os.system( 'mkdir -p %s' % ( writPath ) ) # --- create folder
-        os.system( 'cp utility.py LammpsPostProcess2nd.py OvitosCna.py %s' % ( writPath ) ) #--- cp python module
+#        os.system( 'cp utility.py LammpsPostProcess2nd.py OvitosCna.py %s' % ( writPath ) ) #--- cp python module
         makeOAR( writPath, 1, 1, durtn, PYFIL, argv+"/Run%s"%counter) # --- make oar script
         os.system( 'chmod +x oarScript.sh; mv oarScript.sh %s; cp config.ini %s;cp %s/%s %s' % ( writPath, writPath, EXEC_DIR, PYFIL, writPath ) ) # --- create folder & mv oar scrip & cp executable
         os.system( 'sbatch --partition=%s --mem=%s --time=%s --job-name %s.%s --output %s.%s.out --error %s.%s.err \
