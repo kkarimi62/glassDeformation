4fc2628284249c450085bd412b9178774e663726
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-07-25 11:32:56 +0200

    update

--- simulations/simulations.py
+++ simulations/simulations.py
@@ -24,22 +24,23 @@ if __name__ == '__main__':
 
     runs	 = [0,1,2]
     #
-    nThreads = 16 #8 #2
+    nThreads = 1 #16 #8 #2
     nNode	 = 1
     #
     jobname  = {
                 1:'CuZrNatom32KT300Tdot1E-3Sheared',
                 2:'CuZrNatom32KT300Tdot1E-1Elasticity',
                 4:'ElasticityT300/Co5Cr2Fe40Mn27Ni26/itime0',
-                5:'shear/quenched/glassCo5Cr2Fe40Mn27Ni26',
-               }[5]
+                5:'annealing/glassCo5Cr2Fe40Mn27Ni26',
+                6:'annealingMultiAge/glassCo5Cr2Fe40Mn27Ni26',
+               }[6]
     sourcePath = os.getcwd() +\
                 {	0:'/junk',
                     1:'/../postprocess/NiCoCrNatom1K',
                     2:'/CuZrNatom32KT300Tdot1E-1Sheared',
                     3:'/glass/glassCo5Cr2Fe40Mn27Ni26',
                     5:'/annealing/glassCo5Cr2Fe40Mn27Ni26',
-                }[3] #--- must be different than sourcePath
+                }[5] #--- must be different than sourcePath
         #
     sourceFiles = { 0:False,
                     1:['Equilibrated_300.dat'],
@@ -47,9 +48,10 @@ if __name__ == '__main__':
                     4:['data_minimized.txt'],
                     6:['data.0.txt','dumpSheared.xyz'], 
                     3:['data.0.txt','Co5Cr2Fe40Mn27Ni26_glass.dump','Co5Cr2Fe40Mn27Ni26.txt'], 
-                    7:['Co5Cr2Fe40Mn27Ni26_glass.data'], 
                     5:['swapped.dat'], #--- only one partition! for multiple ones, use 'submit.py'
-                 }[7] #--- to be copied from the above directory
+                    7:['Co5Cr2Fe40Mn27Ni26_glass.data'],
+                    8:['traj.dump'],
+                 }[8] #--- to be copied from the above directory
     #
     EXEC_DIR = '/home/kamran.karimi1/Project/git/lammps2nd/lammps/src' #--- path for executable file
     #
@@ -78,6 +80,7 @@ if __name__ == '__main__':
                     'p0':'partition.py',
                     'p1':'WriteDump.py',
                     'p2':'DislocateEdge.py',
+                    'p3':'splitDump.py',
                 } 
     #
     Variable = {
@@ -85,7 +88,7 @@ if __name__ == '__main__':
                 4:' -var T 600 -var t_sw 20.0 -var DataFile Equilibrated_600.dat -var nevery 1000 -var ParseData 1 -var WriteData swapped_600.dat', 
                 5:' -var buff 0.0 -var nevery 1000 -var ParseData 0 -var natoms 10000 -var ntype 2 -var cutoff 3.54  -var DumpFile dumpMin.xyz -var WriteData data_minimized.dat -var seed0 %s -var seed1 %s -var seed2 %s -var seed3 %s'%tuple(np.random.randint(1001,9999,size=4)), 
                 6:' -var buff 0.0 -var T 300.0 -var GammaXY 0.2 -var GammaDot 1.0e-04 -var ndump 100 -var ParseData 1 -var DataFile equilibrated.dat -var DumpFile dumpSheared.xyz -var WriteData sheared.dat -var thermoFile thermo-shear.txt',
-                7:' -var buff 0.0 -var T 300 -var P 0.0 -var nevery 1000 -var ParseData 1 -var DataFile Co5Cr2Fe40Mn27Ni26_glass.dat -var DumpFile dumpThermalized.xyz -var WriteData equilibrated.dat -var thermoFile thermo_thermalized.txt',
+                7:' -var buff 0.0 -var T 300 -var P 0.0 -var nevery 1000 -var ParseData 1 -var DataFile Co5Cr2Fe40Mn27Ni26_glass.data -var DumpFile dumpThermalized.xyz -var WriteData equilibrated.dat -var thermoFile thermo_thermalized.txt',
                 8:' -var buff 3.0 -var T 0.1 -var sigm 1.5 -var sigmdt 0.01 -var ParseData 1 -var DataFile Equilibrated_300.dat -var DumpFile dumpSheared.xyz',
                 9:' -var natoms 1000 -var cutoff 3.52 -var ParseData 1',
                 10:' -var T 300.0 -var teq	1.0	-var up -1.0e-03 -var nevery 50 -var ParseData 1 -var DataFile data.0.txt -var DumpFile dumpUp_',
@@ -95,6 +98,7 @@ if __name__ == '__main__':
                 'p0':' swapped_600.dat 10.0 %s'%(os.getcwd()+'/../postprocess'),
                 'p1':' swapped_600.dat ElasticConst.txt DumpFileModu.xyz %s'%(os.getcwd()+'/../postprocess'),
                 'p2':' %s 3.52 40.0 20.0 40.0 data.txt'%(os.getcwd()+'/../postprocess'),
+                'p3':' traj.dump',
                 } 
     #--- different scripts in a pipeline
     indices = {
@@ -102,9 +106,10 @@ if __name__ == '__main__':
                 3:[11], #--- elastic moduli at finite T: laos
                 1:[5,7,0,6], #--- minimize, thermalize,melt & quench, shear
                 0:[5,7,0,13], #--- minimize, thermalize,melt & quench, anneal
-                4:[13], #--- anneal
                 5:[7,6], #--- shear
-              }[5]
+                4:[13], #--- anneal
+                6:['p3'], #--- data files based on glass age
+              }[6]
     Pipeline = list(map(lambda x:LmpScript[x],indices))
     Variables = list(map(lambda x:Variable[x], indices))
     EXEC = list(map(lambda x:'lmp' if type(x) == type(0) else 'py', indices))	
