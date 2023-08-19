25ccabd006dbb6e2cb85ebc5eb9608bd26f00a19
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-08-19 20:00:47 +0200

    update

--- python/python.py
+++ python/python.py
@@ -37,7 +37,8 @@ if __name__ == '__main__':
                 '5':'D2minAnalysisT300/Co5Cr2Fe40Mn27Ni26', 
                 '7':'annealing/glassCo5Cr2Fe40Mn27Ni26', 
                 '8':'shear/glassCo5Cr2Fe40Mn27Ni26/age0/itime0', 
-                }['8']
+                '9':'hmodu/glassCo5Cr2Fe40Mn27Ni26/age0/df0', 
+                }['9']
     DeleteExistingFolder = True
     readPath = os.getcwd() + {
                                 '1':'/../testRuns/Preparation/ElasticityT300/Co5Cr2Fe40Mn27Ni26/itime0',
--- python/submitPY.py
+++ python/submitPY.py
@@ -3,41 +3,46 @@ if __name__ == '__main__':
     import os
     import numpy as np
     #---
-    lnums = [ 39, 48, 8   ]
+    lnums = [ 40, 49, 10   ]
     string=open('python.py').readlines() #--- python script
     Age ={
             0:0,
-#             1:1,
+             1:1,
              2:2,
-#             3:3,
+             3:3,
              4:4,
-#             5:5,
+             5:5,
              6:6,
-#             7:7,
+             7:7,
              8:8,
-#             9:9,
+             9:9,
          }
     
     #--- timestpes
     ndump =100;GammaXY=0.2;dt=1e-3;GammaDot=1.0e-04;
     Nstep=np.floor(GammaXY/dt/GammaDot);Nevery=np.ceil(Nstep/ndump)
     Times = dict(zip(range(ndump+1), np.arange(0,Nstep+Nevery,Nevery,dtype=int)))
+
+    DF={0:128,1:256,2:512}
     
     #--- 
     count = 0
     for key_age in Age:
-        for key_t in Times:
-            if key_t % 4 == 0:
+          for key_d in DF:
+#         for key_t in Times:
+#             if key_t % 4 == 0:
             #---	
             #---	densities
                 inums = lnums[ 0 ] - 1
-                string[ inums ] = "\t\'8\':\'shear/glassCo5Cr2Fe40Mn27Ni26/age%s/itime%s\',\n"%(key_age,key_t) #--- change job name
+#                string[ inums ] = "\t\'8\':\'shear/glassCo5Cr2Fe40Mn27Ni26/age%s/itime%s\',\n"%(key_age,key_t) #--- change job name
+                string[ inums ] = "\t\'9\':\'hmodu/glassCo5Cr2Fe40Mn27Ni26/age%s/df%s\',\n"%(key_age,key_d) #--- change job name
             #---
                 inums = lnums[ 1 ] - 1
                 string[ inums ] = "\t\'8\':\'/../simulations/shear/glassCo5Cr2Fe40Mn27Ni26/age%s\',\n"%(key_age) #--- change job name
             #---
                 inums = lnums[ 2 ] - 1
-                string[ inums ] = "    confParser.set(\'parameters\',\'itime\',\'%d\')\n"%(Times[key_t]) #--- change job name
+#                string[ inums ] = "    confParser.set(\'parameters\',\'itime\',\'%d\')\n"%(Times[key_t]) #--- change job name
+                string[ inums ] = "    confParser.set(\'Spline\',\'deg_f\',\'%d\')\n"%(DF[key_d]) #--- change job name
             #---
                 sfile=open('junk%s.py'%count,'w');sfile.writelines(string);sfile.close()
                 os.system( 'python3 junk%s.py'%count )
