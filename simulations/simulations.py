72040f420affb08c0f0c810e1e45af59fddbc0e6
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-09-12 09:59:38 +0200

    update

--- simulations/simulations.py
+++ simulations/simulations.py
@@ -119,7 +119,7 @@ if __name__ == '__main__':
     EXEC_lmp = ['lmp_mpi','lmp_serial'][0]
     durtn = ['167:59:59','96:59:59','00:59:59'][0]
     mem = '8gb'
-    partition = ['gpu-v100','parallel','cpu2019','single'][2]
+    partition = ['gpu-v100','parallel','cpu2019','single'][1]
     #---
     if DeleteExistingFolder:
         os.system( 'rm -rf %s' % jobname ) #--- rm existing
