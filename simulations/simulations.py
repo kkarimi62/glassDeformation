81e709e17426ec6aa17ae820569dcebb2e86ade5
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2024-02-29 11:33:19 -0800

    irradiation implemented!

--- simulations/simulations.py
+++ simulations/simulations.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
     DeleteExistingFolder = True
     #
     EXEC_lmp = ['lmp_mpi','lmp_serial'][0]
-    durtn = ['167:59:59','96:59:59','00:59:59'][2]
+    durtn = ['167:59:59','96:59:59','00:59:59'][0]
     mem = '8gb'
     partition = ['gpu-v100','parallel','cpu2019','single'][2]
     #---
